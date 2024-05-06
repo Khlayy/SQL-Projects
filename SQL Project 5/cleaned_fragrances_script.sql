758fcf7d8aca4f1302dc9b70b2d340e29a5a978d
Jah <167051447+Khlayy@users.noreply.github.com>
2024-05-06 13:16:18 +0800

    Update cleaned_fragrances_script.sql

--- SQL Project 5/cleaned_fragrances_script.sql
+++ SQL Project 5/cleaned_fragrances_script.sql
@@ -1,6 +1,6 @@
 -- DATA CLEANING
 
-SELECT * FROM safe_chemicals.`fragrances v2`;
+SELECT * FROM safe_chemicals.fragrances;
 
 
 -- Removing brackets in the List Call column
@@ -10,19 +10,19 @@ SET `List Call` =
 
 
 -- Removing asterisk in the Caveat - Chemical Use column
-UPDATE `fragrances v2`
+UPDATE fragrances
 SET `Caveat - Chemical Use` = (
 	SELECT SUBSTRING_INDEX(`Caveat - Chemical Use`, '*', -1)
 	);
 
 
 -- Replacing 'see above/See above' with 'see notes below/See notes below'
-UPDATE `fragrances v2`
+UPDATE fragrances
 SET `Caveat - Chemical Use` = 
 	REPLACE(`Caveat - Chemical Use`, 'see above', 'see notes below')
 	WHERE `Caveat - Chemical Use` LIKE '%see above%';
 
-UPDATE `fragrances v2`
+UPDATE fragrances
 SET `Caveat - Chemical Use` = 
 	REPLACE(`Caveat - Chemical Use`, 'See above', 'See notes below')
 	WHERE `Caveat - Chemical Use` LIKE '%See above%';
